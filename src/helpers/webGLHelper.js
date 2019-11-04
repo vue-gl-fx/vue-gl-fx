21da58a7df39397a1ade0f03b4d7d03f50a805a1
Nicolas Chesné <nc@lachose.fr>
2019-11-04 14:53:50 +0100

    Moving initTexture to sampler2D type. Watching vflip changes

--- src/helpers/webGLHelper.js
+++ src/helpers/webGLHelper.js
@@ -41,22 +41,4 @@ export const initQuadBuffer = (gl) => {
   return buffer;
 };
 
-export const initTexture = (gl, image, autoMipmap = true, vflip=false) => {
-  const isPowerOf2 = value => (value && (value - 1)) === 0;
-  const texture = gl.createTexture();
 
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  if(vflip){
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  }
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-
-  if (autoMipmap && isPowerOf2(image.width) && isPowerOf2(image.height)) {
-    gl.generateMipmap(gl.TEXTURE_2D);
-  } else {
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  }
-  return texture;
-};
