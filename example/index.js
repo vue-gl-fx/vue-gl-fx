48d7eba0f754f8c1c494ea0c9d165c17ab8d073c
Nicolas Chesné <nc@lachose.fr>
2019-11-04 13:08:24 +0100

    Moving to plugin architecture

--- example/index.js
+++ example/index.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import App from './App'
-import {GlFx, GlFxUniform, GlFxResolution, GlFxTime, GlFxFrame} from '../src/index'
+import VueGlFx from '../src/index';
 
- Vue.component('gl-fx', GlFx)
- Vue.component('gl-fx-uniform', GlFxUniform)
- Vue.component('gl-fx-resolution', GlFxResolution)
- Vue.component('gl-fx-time', GlFxTime)
- Vue.component('gl-fx-frame', GlFxFrame)
+Vue.use(VueGlFx)
 
 /* eslint-disable no-new */
 new Vue({
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import GlFxUniform from './components/uniforms/GlFxUniform.vue'
 import GlFxResolution from './components/uniforms/GlFxResolution.vue'
 import GlFxTime from './components/uniforms/GlFxTime.vue'
 import GlFxFrame from './components/uniforms/GlFxFrame.vue'
+import GlFxMouse from './components/uniforms/GlFxMouse.vue'
 
-export {
-    GlFx,
-    GlFxUniform,
-    GlFxResolution,
-    GlFxTime,
-    GlFxFrame,
+const VueGlFx = {
+
+}
+VueGlFx.install = function (Vue) {
+    Vue.component('gl-fx', GlFx)
+    Vue.component('gl-fx-uniform', GlFxUniform)
+    Vue.component('gl-fx-resolution', GlFxResolution)
+    Vue.component('gl-fx-time', GlFxTime)
+    Vue.component('gl-fx-frame', GlFxFrame)
+    Vue.component('gl-fx-mouse', GlFxMouse)
 }
 
-export default GlFx;
+export default VueGlFx;
