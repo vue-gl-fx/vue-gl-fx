fd3523909fe44619e62455ec00c9bcc448b9ff4e
Nicolas Chesné <nc@lachose.fr>
2019-11-05 12:40:11 +0100

    poi should use the esm version

--- poi.config.js
+++ poi.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   chainWebpack(config, context) { //eslint-disable-line no-unused-vars
     config
-      .entry('index')
+      .entry('index.esm')
       .add('./example/index.js')
       .end()
       .output.path(path.resolve(process.cwd(), './demo'))
